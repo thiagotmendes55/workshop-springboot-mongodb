348ff456f37490f900caaab9d8729cc8858c711a
thiagotmendes55 <thiagotmendes@gmail.com>
2020-04-21 22:10:26 -0300

    Obtendo usuario com ID e tratamento de not found

--- src/main/java/com/mendes/curso/resources/UserResource.java
+++ src/main/java/com/mendes/curso/resources/UserResource.java
@@ -1,17 +1,18 @@
-package com.mendes.curso.resource;
+package com.mendes.curso.resources;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mendes.curso.domain.User;
 import com.mendes.curso.dto.UserDTO;
-import com.mendes.curso.service.UserService;
+import com.mendes.curso.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
@@ -26,5 +27,13 @@ public class UserResource {
 		List<UserDTO> usuariosDTO = usuarios.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		
 		return ResponseEntity.ok().body(usuariosDTO);		
+	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> listaUm(@PathVariable String id) {
+		User usuario = service.listaUm(id);
+		UserDTO usuarioDTO = new UserDTO(usuario);
+		
+		return ResponseEntity.ok().body(usuarioDTO);		
 	}
 }
--- src/main/java/com/mendes/curso/services/UserService.java
+++ src/main/java/com/mendes/curso/services/UserService.java
@@ -1,12 +1,14 @@
-package com.mendes.curso.service;
+package com.mendes.curso.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mendes.curso.domain.User;
 import com.mendes.curso.repository.UserRepository;
+import com.mendes.curso.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> listaTodos() {
 		return repo.findAll();
+	}
+	
+	public User listaUm(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
